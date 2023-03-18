7d5884e9839df9ccee43cf38ddb9d5c357ac0fe8
Petru Dimulescu <petru.dimulescu@gmail.com>
2023-03-18 06:59:24 +0200

    gradle working but swagger disabled

--- src/main/java/murraco/configuration/SwaggerConfig.java
+++ src/main/java/murraco/configuration/SwaggerConfig.java
@@ -22,10 +22,10 @@ import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
+//@Configuration
+//@EnableSwagger2
 public class SwaggerConfig {
 
   @Bean
@@ -33,7 +33,7 @@ public class SwaggerConfig {
     return new Docket(DocumentationType.SWAGGER_2)//
         .select()//
         .apis(RequestHandlerSelectors.any())//
-        .paths(Predicates.not(PathSelectors.regex("/error")))//
+        // .paths(Predicates.not(PathSelectors.regex("/error")))//
         .build()//
         .apiInfo(metadata())//
         .useDefaultResponseMessages(false)//
